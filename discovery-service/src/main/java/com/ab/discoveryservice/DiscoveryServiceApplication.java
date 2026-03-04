f2092f3366a2c6788fd3be84433514cac7c1abd6
iammmila <ibramila2002@gmail.com>
2026-03-04 20:17:49 +0100

    discovery-service: comments are completed

--- discovery-service/src/main/java/com/ab/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/ab/discoveryservice/DiscoveryServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer // Turns this Spring Boot app into a Eureka Service Registry (other services register + discover each other here)
 public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
